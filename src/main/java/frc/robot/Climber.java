062c4e445ac0f25f70c2184ea31d53e2b40c54ba
nickquednow <56178732+nickquednow@users.noreply.github.com>
2020-02-29 18:21:52 -0500

    make code beautiful again
    look at camera commit for details

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -3,16 +3,24 @@ package frc.robot;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 public class Climber{
-    Robot robot;
+    Robot robot; //allows communication to different classes
+
+    //instanciates class
     public Climber(Robot robot){
         this.robot = robot;
     }
+
+    //disables solenoid
     public void off(){
         robot.pneumatics.climberSolenoid.set(kOff);
     }
+
+    //extends pneumatic for climber
     public void up(){
         robot.pneumatics.climberSolenoid.set(kForward);
     }
+
+    //retracts pneumatic for climber
     public void down(){
         robot.pneumatics.climberSolenoid.set(kReverse);
     }
