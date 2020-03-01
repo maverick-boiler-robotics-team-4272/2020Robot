1f44ec6e8003283fb7e8b117d53e34b2722e0067
nickquednow <56178732+nickquednow@users.noreply.github.com>
2020-02-29 20:16:07 -0500

    make code beautiful again

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -6,25 +6,34 @@ import com.revrobotics.ControlType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public class Intake{
-    Robot robot;
+    Robot robot;//makes sure that it can access the rest of the robot
+
+    //instanciates the class
     public Intake(Robot robot){
         this.robot = robot;
     }
+
+    // turns off the motors for the intake
     public void off(){
         robot.pneumatics.intakeSolenoid.set(kOff);
         robot.motor.intake.set(0);
         robot.motor.intake2.set(0);
     }
+
+    // extends the pneumatic for the intake
     public void out(){
         robot.pneumatics.intakeSolenoid.set(kReverse);
     }
+
+    // retracts the pneumatic for the intake
     public void in(){
         robot.pneumatics.intakeSolenoid.set(kForward);
         robot.motor.intake.set(0);
         robot.motor.intake2.set(0);
     }
+
+    // turns on the motors for the intake
     public void on(double speed){
-        //robot.motor.intakePID.setReference(4000, ControlType.kVelocity);
         robot.motor.intake.set(speed);
         robot.motor.intake2.set(-speed);
     }
