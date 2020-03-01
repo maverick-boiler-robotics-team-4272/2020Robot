e947b728e687ec63f43fd194a693bdccfccba676
nickquednow <56178732+nickquednow@users.noreply.github.com>
2020-02-29 20:09:53 -0500

    simplified code
    this code was already beautiful

--- src/main/java/frc/robot/HwJoystick.java
+++ src/main/java/frc/robot/HwJoystick.java
@@ -9,11 +9,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
-/**
- * Add your docs here.
- */
 public class HwJoystick {
+    //tank drive controlls for driver
     public final Joystick leftJoystick = new Joystick(0);
     public final Joystick rightJoystick = new Joystick(1);
+
+    //xbox controller for operator
     public final XboxController xbox = new XboxController(2);
 }
