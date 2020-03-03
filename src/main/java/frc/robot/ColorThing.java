8158e42f6c0d1c62ef8dfe818b0b4f7f56498559
nickquednow <nqued66@tscstudents.net>
2020-03-03 15:45:04 -0500

    Revert "changed colorwheel to use encoder counts"
    This reverts commit 811aba3f444bb2c75862bf8925f99b3e7c18329e.

--- src/main/java/frc/robot/ColorThing.java
+++ src/main/java/frc/robot/ColorThing.java
@@ -10,183 +10,122 @@ import com.revrobotics.ColorMatch;
 
 
 public class ColorThing {
-	Robot robot;//gives access to the motors and network tables
+  Robot robot;
 
-	//declares what the color sensor is and that it is on the I2C port
-	private final I2C.Port i2cPort = I2C.Port.kOnboard;
-	private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
-	//starts matching colors, and has RGB values for the colors on the wheel
-	private final ColorMatch m_colorMatcher = new ColorMatch();
-	private final Color kBlueTarget = ColorMatch.makeColor(0.21, 0.44, 0.3);
-	private final Color kGreenTarget = ColorMatch.makeColor(0.2, 0.6, 0.2);
-	private final Color kRedTarget = ColorMatch.makeColor(0.67, 0.27, 0.0);
-	private final Color kYellowTarget = ColorMatch.makeColor(0.42, 0.51, 0.0);
-	private final Color kFakeYellowTarget = ColorMatch.makeColor(0.6, 0.31, 0.05);
-	private final Color kFakeGreenTarget = ColorMatch.makeColor(0.3, 0.48, 0.22);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.21, 0.44, 0.3);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.2, 0.6, 0.2);
+  private final Color kRedTarget = ColorMatch.makeColor(0.67, 0.27, 0.0);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.42, 0.51, 0.0);
+  private final Color kFakeYellowTarget = ColorMatch.makeColor(0.6, 0.31, 0.05);
+  private final Color kFakeGreenTarget = ColorMatch.makeColor(0.3, 0.48, 0.22);
 
+  private NetworkTableInstance inst = NetworkTableInstance.getDefault();
+  private NetworkTable table = inst.getTable("datatable");
+  NetworkTableEntry colorSensor = table.getEntry("colorSensor");
+  NetworkTableEntry rawColorSensor_r = table.getEntry("rawColorSensor_r");
+  NetworkTableEntry rawColorSensor_g = table.getEntry("rawColorSensor_g");
+  NetworkTableEntry rawColorSensor_b = table.getEntry("rawColorSensor_b");
+  NetworkTableEntry proximit = table.getEntry("proximity");
+  NetworkTableEntry go_stop = table.getEntry("go_stop");
 
-	//declares our network table entries for simplicity
-	NetworkTableEntry colorSensor = robot.tables.colorSensor;
-	NetworkTableEntry rawColorSensor_r = robot.tables.rawColorSensor_r;
-	NetworkTableEntry rawColorSensor_g = robot.tables.rawColorSensor_g;
-	NetworkTableEntry rawColorSensor_b = robot.tables.rawColorSensor_b;
-	NetworkTableEntry proximit = robot.tables.proximit;
-	NetworkTableEntry go_stop = robot.tables.go_stop;
+  private int y_itr = 10;
+  private int b_itr = 10; 
+  private int g_itr = 10;
+  private int r_itr = 10; 
+  private int rotations = 8;
+  private String current_color;
+  public boolean is_spinning;
 
-	//how many times the color has been hit on the wheel
-	private int y_itr = 10;
-	private int b_itr = 10; 
-	private int g_itr = 10;
-	private int r_itr = 10; 
+  public ColorThing(Robot robot) {
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);
+    m_colorMatcher.addColorMatch(kFakeYellowTarget);
+    m_colorMatcher.addColorMatch(kFakeGreenTarget);
+    this.robot = robot;
+    is_spinning = false;
+  }
 
-	//number of times we want the color panel to spin times two
-	private int rotations = 8;
+  public void colorRotation(boolean reset) {
+    // control panel
+    Color detectedColor = m_colorSensor.getColor();
+    /**
+     * Run the color match algorithm on our detected color
+     */
+    String colorString;
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
-	//stores the current color of the wheel
-	private String current_color;
-
-	//lets us know if the wheel is supposed to be spinning
-	public boolean is_spinning;
-
-	
-	private boolean extended = false;
-
-
-	//instanciates this class and makes sure that the code can match to the correct color
-	public ColorThing(Robot robot) {
-		m_colorMatcher.addColorMatch(kBlueTarget);
-		m_colorMatcher.addColorMatch(kGreenTarget);
-		m_colorMatcher.addColorMatch(kRedTarget);
-		m_colorMatcher.addColorMatch(kYellowTarget);
-		m_colorMatcher.addColorMatch(kFakeYellowTarget);
-		m_colorMatcher.addColorMatch(kFakeGreenTarget);
-		this.robot = robot;
-
-		//when the robot starts up set the CPM to be not spinning
-		is_spinning = false;
-	}
-
-	public void loop(){
-		//loopy type things
-	}
-
-	public void reset(){
-		//things to do once
-	}
-
-	public void toggle(){
-		if(extended){
-			down();
-			extended = false;
-		}else{
-			up();
-			extended = true;
-		}
-	}
-
-	public void up(){
-		robot.pneumatics.CPMSolenoid.set(kForward);
-	}
-
-	public void down(){
-		robot.pneumatics.CPMSolenoid.set(kReverse);
-	}
-
-	public void colorRotation(boolean reset) {
-		//gets the current RGB value fo the color sensor
-		Color detectedColor = m_colorSensor.getColor();
-
-		//gets the closest atched color according to the RGB values from above
-		String colorString;
-		ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
-
-		
-		//makes sure that the color is human readable
-		if (match.color == kBlueTarget) {
-			colorString = "Blue";
-		} else if (match.color == kRedTarget) {
-			colorString = "Red";
-		} else if (match.color == kGreenTarget) {
-			colorString = "Green";
-		} else if (match.color == kYellowTarget) {
-			colorString = "Yellow";
-		} else if (match.color == kFakeYellowTarget) {
-			colorString = "Fake Yellow";
-		} else if (match.color == kFakeGreenTarget) {
-			colorString = "Fake Green";
-		} else {
-			//you done screwed up if you get this
-			colorString = "Unknown";
-		}
-
-		//publish the current color to network tables
-		colorSensor.setString(colorString);
-
-		//publish raw RGB values to network tables 
-		rawColorSensor_r.setDouble(detectedColor.red);
-		rawColorSensor_g.setDouble(detectedColor.green);
-		rawColorSensor_b.setDouble(detectedColor.blue);
-
-		//publish how close the color sensor is to the color wheel
-		proximit.setDouble(m_colorSensor.getProximity());
-
-		//tests for the current color, then checks if it was changed if it was not then add one to the given color
-		if (colorString == "Yellow") {
-			if (current_color != colorString) {// tests if the color has changed to this
-				current_color = colorString; //sets the color to be this so we do not repeatedly add one to our integer
-				y_itr += 1; //if yes then add one to the color
-			}
-		}
-		if (colorString == "Blue") { // same as above
-			if (current_color != colorString) {
-				current_color = colorString;
-				b_itr += 1;
-			}
-		}
-		if (colorString == "Green") { // same as above
-			if (current_color != colorString) {
-				current_color = colorString;
-				g_itr += 1;
-			}
-		}
-		if (colorString == "Red") { // same as above
-			if (current_color != colorString) {
-				current_color = colorString;
-				r_itr += 1;
-			}
-		}
-
-		//tests if the number of rotation has exceeded the set amount
-		if (y_itr >= rotations || b_itr >= rotations || g_itr >= rotations || r_itr >= rotations) {
-			//tells the operator that the CPM has stopped
-			go_stop.setString("STOP");
-			//stops the CPM
-			is_spinning=false;
-		} else {
-			//if it has not reached the target rotations tell the driver that it is still going
-			go_stop.setString("Keep going");
-		}
-
-		//if the reset signal has been called than set all variables to 0 so we can rerun this code if needbe
-		if (reset) {
-			y_itr = 0;
-			b_itr = 0;
-			g_itr = 0;
-			r_itr = 0;
-			is_spinning = true;
-		}
-
-		//spin the CPM if we are good to go
-		if(is_spinning) {
-      robot.motor.CPM.set(0.3);
-      System.out.println("CPM Encoder counts: " + robot.motor.CPM.getEncoder().getPosition());
+    
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else if (match.color == kFakeYellowTarget) {
+      colorString = "Fake Yellow";
+    } else if (match.color == kFakeGreenTarget) {
+      colorString = "Fake Green";
     } else {
-     robot.motor.CPM.set(0);
+      colorString = "Unknown";
     }
-    //Starting encoder counts : 1562.692
-    //Ending encoder counts : 1750.139
- }
+    colorSensor.setString(colorString);
+    rawColorSensor_r.setDouble(detectedColor.red);
+    rawColorSensor_g.setDouble(detectedColor.green);
+    rawColorSensor_b.setDouble(detectedColor.blue);
+    proximit.setDouble(m_colorSensor.getProximity());
+
+    if (colorString == "Yellow") {
+      if (current_color != colorString) {
+        current_color = colorString;
+        y_itr += 1;
+      }
+    }
+    if (colorString == "Blue") {
+      if (current_color != colorString) {
+        current_color = colorString;
+        b_itr += 1;
+      }
+    }
+    if (colorString == "Green") {
+      if (current_color != colorString) {
+        current_color = colorString;
+        g_itr += 1;
+      }
+    }
+    if (colorString == "Red") {
+      if (current_color != colorString) {
+        current_color = colorString;
+        r_itr += 1;
+      }
+    }
+    if (y_itr >= rotations || b_itr >= rotations || g_itr >= rotations || r_itr >= rotations) {
+      go_stop.setString("STOP");
+      is_spinning=false;
+    } else {
+      go_stop.setString("Keep going");
+    }
+    if (reset) {
+      y_itr = 0;
+      b_itr = 0;
+      g_itr = 0;
+      r_itr = 0;
+      is_spinning = true;
+    }
+    
+    if(is_spinning) {
+       robot.motor.CPM.set(0.3);
+     } else {
+      robot.motor.CPM.set(0);
+     }
+  }
 
 	public void colorSelection(String color /*recieved color from FMS*/) {//stage 3 of the match for color selection from FMS
 
